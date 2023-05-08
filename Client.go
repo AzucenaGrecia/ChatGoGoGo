977cf380cf23e40ef1c56a69bd0598edf62ac675
Grecia Delgado <grecia.delgado@kushkipagos.com>
2023-05-07 21:00:56 -0500

    Implement chat btw twoo computers

--- Client.go
+++ Client.go
@@ -3,16 +3,31 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 )
 
 var done chan interface{}
 var interrupt chan os.Signal
+var clien_user_name string
+var mode string
+
+type SocketMessage struct {
+	Username    string
+	MessageType int // 1. Login // 2 Chat // 3 list users in chat
+	Text        string
+}
+
+type SocketResponse struct {
+	MessageType int // 1. Login // 2 Chat // 3 list users in chat
+	Text        string
+}
 
 func receiveHandler(connection *websocket.Conn) {
 	defer close(done)
@@ -22,14 +37,94 @@ func receiveHandler(connection *websocket.Conn) {
 			log.Println("Error in receive:", err)
 			return
 		}
-		log.Printf("Received: %s\n", msg)
+
+		var response SocketResponse
+		json.Unmarshal(msg, &response)
+
+		switch response.MessageType {
+		case 2:
+			if mode == "CHAT" {
+				fmt.Println(response.Text)
+			}
+		case 3:
+			fmt.Println(response.Text)
+			printMenu()
+		}
+
 	}
 }
 
+func welcome_and_login() string {
+	fmt.Println("Hola Bienvenido al chat")
+	fmt.Println("Ingresa tu nombre de usuario:")
+	reader := bufio.NewReader(os.Stdin)
+	userName, _ := reader.ReadString('\n')
+
+	return userName
+}
+
+func login(conn *websocket.Conn) {
+	message := SocketMessage{
+		Username:    clien_user_name,
+		MessageType: 1,
+		Text:        "",
+	}
+	bytes_message, _ := json.Marshal(message)
+	err_socket := conn.WriteMessage(websocket.TextMessage, bytes_message)
+	if err_socket != nil {
+		log.Println("Error during writing to websocket:", err_socket)
+	}
+	fmt.Println("Logged in the chat ")
+}
+
+func sendMessage(conn *websocket.Conn, text string) {
+	message := SocketMessage{
+		Username:    clien_user_name,
+		MessageType: 2,
+		Text:        text,
+	}
+
+	bytes_message, _ := json.Marshal(message)
+	err_socket := conn.WriteMessage(websocket.TextMessage, bytes_message)
+	if err_socket != nil {
+		log.Println("Error during writing to websocket:", err_socket)
+	}
+}
+
+func listUsers(conn *websocket.Conn) {
+	message := SocketMessage{
+		Username:    clien_user_name,
+		MessageType: 3,
+		Text:        "",
+	}
+	bytes_message, _ := json.Marshal(message)
+	err_socket := conn.WriteMessage(websocket.TextMessage, bytes_message)
+	if err_socket != nil {
+		log.Println("Error during writing to websocket:", err_socket)
+	}
+}
+
+func makeCommand(command string) {
+	cmd := exec.Command(command) //Linux example, its tested
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func printMenu() {
+	fmt.Println("Ejecuta un comando:")
+	fmt.Println("1. Listar usuarios")
+	fmt.Println("2. Chat en vivo")
+}
 func main() {
+	mode = "MENU"
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Error env")
 	}
+	reader := bufio.NewReader(os.Stdin)
+
+	username := welcome_and_login()
+	clien_user_name = username
+
 	fmt.Println("App will connect to ..." + os.Getenv("HOST") + ":" + os.Getenv("PORT"))
 
 	done = make(chan interface{})    // Channel to indicate that the receiverHandler is done
@@ -45,17 +140,35 @@ func main() {
 	defer conn.Close()
 	go receiveHandler(conn)
 
-	// Our main loop for the client
-	// We send our relevant packets here
+	//LOGIN TO CHAT
+	login(conn)
 
-	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Print("Text to send: ")
+		if mode == "MENU" {
+			printMenu()
+			input, _ := reader.ReadString('\n')
+			command := input[0:1]
 
-		input, _ := reader.ReadString('\n')
-		err_socket := conn.WriteMessage(websocket.TextMessage, []byte(input))
-		if err_socket != nil {
-			log.Println("Error during writing to websocket:", err_socket)
+			switch command {
+			case "1":
+				fmt.Println("Listar usuarios")
+				listUsers(conn)
+			case "2":
+				mode = "CHAT"
+			default:
+				fmt.Println("No command")
+			}
+		}
+		if mode == "CHAT" {
+			fmt.Println("Text to send:  (Q to quit)")
+			input, _ := reader.ReadString('\n')
+			command := input[0:2]
+			if command == "q\n" {
+				mode = "MENU"
+				makeCommand("clear")
+			} else {
+				sendMessage(conn, input)
+			}
 		}
 
 	}
--- Server.go
+++ Server.go
@@ -2,24 +2,104 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gorilla/websocket"
-	"github.com/joho/godotenv"
 )
 
 type UserConnected struct {
 	Username string
 	Conn     *websocket.Conn
+	Online   bool
+	exist    bool
+}
+type SocketMessageServer struct {
+	Username    string
+	MessageType int // 1. Login // 2 Chat
+	Text        string
+}
+type SocketResponseServer struct {
+	MessageType int // 1. Login // 2 Chat // 3 list users in chat
+	Text        string
 }
 
 var upgrader = websocket.Upgrader{} // use default options
-var users [10]UserConnected
+var users [100]UserConnected
 var clients map[*websocket.Conn]bool
 
+func findUser(username string) UserConnected {
+	for _, user := range users {
+		if user.Username == username {
+			return user
+		}
+	}
+	return UserConnected{}
+}
+
+func list_all_users() string {
+	list := "Usuarios conectados: \n"
+	for _, user := range users {
+		if user.Online {
+			list += user.Username + "\n"
+		}
+	}
+
+	return list
+}
+
+func logUser(conn *websocket.Conn, username string) bool {
+
+	for i := 0; i < 100; i++ {
+		if !users[i].exist {
+			users[i] = UserConnected{
+				Username: username,
+				Conn:     conn,
+				Online:   true,
+				exist:    true,
+			}
+
+			fmt.Println("Usuario registrado")
+			return true
+		}
+	}
+
+	return false
+}
+
+func validateSocketMessage(conn *websocket.Conn, message []byte) {
+	var recived_message SocketMessageServer
+	err_message_recived := json.Unmarshal(message, &recived_message)
+	if err_message_recived != nil {
+		log.Fatal("ERROR READING MESSAGE")
+	}
+	switch recived_message.MessageType {
+	case 1:
+		logUser(conn, recived_message.Username)
+
+		fmt.Println(users)
+		fmt.Println("Userr logger " + recived_message.Username)
+	case 2: // Send messages to all clients
+		response := SocketResponseServer{
+			MessageType: 2,
+			Text:        recived_message.Username + " -> " + recived_message.Text,
+		}
+		go writeMessage(response)
+		go messageHandler(response)
+	case 3:
+		response := SocketResponseServer{
+			MessageType: 3,
+			Text:        list_all_users(),
+		}
+		writeMessageToUser(response, recived_message.Username)
+
+	default:
+		fmt.Println("No action recived")
+	}
+}
 func socketHandler(w http.ResponseWriter, r *http.Request) {
 	// Upgrade our raw HTTP connection to a websocket based one
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -39,23 +119,33 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 			break // Exit the loop if the client tries to close the connection or the connection with the interrupted client
 		}
 
-		// Send messages to all clients
-		go writeMessage(message)
+		//UNMARSHALL MESSAGE
+		validateSocketMessage(conn, message)
 
-		go messageHandler(message)
 	}
 
 	clients[conn] = false
-	defer conn.Close()
+	conn.Close()
 }
 
-func messageHandler(message []byte) {
-	fmt.Println(string(message))
+func messageHandler(response SocketResponseServer) {
+	fmt.Println(response.Text)
 }
-func writeMessage(message []byte) {
-	for conn := range clients {
-		conn.WriteMessage(websocket.TextMessage, message)
+func writeMessage(response SocketResponseServer) {
+	bytes_message, _ := json.Marshal(response)
+
+	for _, user := range users {
+		if user.Online {
+			user.Conn.WriteMessage(websocket.TextMessage, bytes_message)
+		}
 	}
+}
+
+func writeMessageToUser(response SocketResponseServer, username string) {
+	user_found := findUser(username)
+	bytes_message, _ := json.Marshal(response)
+	user_found.Conn.WriteMessage(websocket.TextMessage, bytes_message)
+
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -65,6 +155,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 func main() {
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Error env")
+	}
+	for i := 0; i < 100; i++ {
+		users[i] = UserConnected{}
 	}
 
 	clients = make(map[*websocket.Conn]bool)
